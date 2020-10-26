00f8913653a4c947c571a49478375c1079422f98
nstarman <nstarman@users.noreply.github.com>
2020-10-26 15:15:22 -0400

    test helpers
    baseclass dependent
    
    Signed-off-by: nstarman <nstarman@users.noreply.github.com>

---
+++ trackstream/tests/helper/tests/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# see LICENSE.rst
+
+"""Test for `~starkman_thesis.tests.helper`."""
+
+__all__ = [
+    # modules
+    "baseclassdependenttests_tests",
+]
+
+
+##############################################################################
+# IMPORTS
+
+# PROJECT-SPECIFIC
+from . import test_baseclassdependenttests as baseclassdependenttests_tests
+
+##############################################################################
+# END
---
+++ trackstream/tests/helper/tests/test_baseclassdependenttests.py
@@ -0,0 +1,101 @@
+# -*- coding: utf-8 -*-
+
+"""Tests for `~starkman_thesis.tests.helper.BaseClassDependentTests`."""
+
+__all__ = [
+    "Test_BaseClassDependentTests",
+]
+
+
+##############################################################################
+# IMPORTS
+
+# THIRD PARTY
+import pytest
+
+# FIRST PARTY
+# PROJECT-SPECIFIC
+from starkman_thesis.tests import helper
+
+##############################################################################
+# CODE
+##############################################################################
+
+
+class Test_BaseClassDependentTests:
+    """Test `~starkman_thesis.tests.helper.BaseClassDependentTests`."""
+
+    @classmethod
+    def setup_class(cls):
+        """Setup fixtures for testing."""
+        cls.klass = helper.BaseClassDependentTests
+
+    # /def
+
+    def test_fail_make_subclass(self):
+        """Test make subclass without passing klass."""
+        with pytest.raises(TypeError) as e:
+            # make class
+            class SubClass(helper.BaseClassDependentTests):
+                pass
+
+        assert "klass" in str(e.value)
+
+    # /def
+
+    def test_make_subclass(self):
+        """Test make subclass with klass."""
+        # make class
+        class SubClass(helper.BaseClassDependentTests, klass=int):
+            pass
+
+        # tests
+        assert SubClass.klass is int
+
+        subclass = SubClass()
+        assert subclass.klass == int
+
+    # /def
+
+    def test_fail_make_subsubclass(self):
+        """Test make subclass without passing klass."""
+        # make class
+        class SubClass(helper.BaseClassDependentTests, klass=int):
+            pass
+
+        with pytest.raises(TypeError) as e:
+
+            class SubSubClass(SubClass):
+                pass
+
+        assert "klass" in str(e.value)
+
+    # /def
+
+    def test_make_subsubclass(self):
+        """Test make sub-subclass with klass."""
+        # make class
+        class SubClass(helper.BaseClassDependentTests, klass=int):
+            pass
+
+        # make subclass
+        class SubSubClass(SubClass, klass=int):
+            pass
+
+        # tests
+        assert SubSubClass.klass is int
+
+        subsubclass = SubSubClass()
+        assert subsubclass.klass == int
+
+    # /def
+
+
+# /class
+
+
+# -------------------------------------------------------------------
+
+
+##############################################################################
+# END
