5135b1b4f42a245de9d337789b49e430decaf486
nstarman <nstarkman@protonmail.com>
2022-10-30 17:05:39 -0700

    remove orb it example
    Signed-off-by: nstarman <nstarkman@protonmail.com>

--- src/trackstream/data/example/__init__.py
+++ src/trackstream/data/example/__init__.py
@@ -1,23 +1,6 @@
 """Example Data."""
 
-__all__ = [
-    # functions & classes
-    "get_example_stream",
-    "get_orbit",
-    "make_ordered_orbit_data",
-    "make_unordered_orbit_data",
-    "make_noisy_orbit_data",
-]
-
-
-##############################################################################
-# IMPORTS
-
 # LOCAL
-from trackstream.data.example.orbit import (
-    get_orbit,
-    make_noisy_orbit_data,
-    make_ordered_orbit_data,
-    make_unordered_orbit_data,
-)
 from trackstream.data.example.stream import get_example_stream
+
+__all__ = ["get_example_stream"]
